f7598cc74eaf83f6e3171f84ea09fe20870483eb
Sandip Murmu <sandipmurmusm@gmail.com>
2020-10-11 05:38:04 +0000

    Chat Room Active. Redis Setup is left

--- audio_player/views.py
+++ audio_player/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "audio_player/index.html")
+    return render(request, "audio_player/index.html", {})
+
+def room(request, room_name):
+    context = { 'chat_room': room_name }
+    return render(request, 'audio_player/room.html', context)
