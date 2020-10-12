c690389b88df0035f4b2b9b3492998f504c40dc8
Abhishek Adhikari <abhiayush23@gmail.com>
2020-10-12 19:41:47 +0530

    updated

--- audio_player/consumers.py
+++ audio_player/consumers.py
@@ -57,8 +57,9 @@ class AudioSession(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message = text_data
+        # text_data_json = json.loads(text_data)
+        # message = text_data_json['message']
 
         # Send message to room group
         await self.channel_layer.group_send(
@@ -74,6 +75,4 @@ class AudioSession(AsyncWebsocketConsumer):
         message = event['message']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=message)
