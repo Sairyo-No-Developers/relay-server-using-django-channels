1dcaa057a1a81d3cdf589bb2d94e510dd52dbc31
Sandip Murmu <sandipmurmusm@gmail.com>
2020-10-14 06:48:58 +0000

    Changes in Session model. Added URL and metadata

--- audio_player/models.py
+++ audio_player/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Session(models.Model):
     session_name = models.TextField(verbose_name = "Session Name", unique = True)
     consumer_name = models.TextField(default="")
+    media_url = models.URLField(verbose_name="Media URL", null = True)
+    metadata = models.TextField(verbose_name="Metadata", default = "")
 
     def __str__(self):
         return self.session_name
